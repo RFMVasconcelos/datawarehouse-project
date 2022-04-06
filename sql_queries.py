441e12f648d952ddd35fa3ca6ed7be9a184b0b10
rui <rui.vasconcelos.mail@gmail.com>
2022-04-06 16:20:50 +0000

    shape CREATE + DROP table queries

--- sql_queries.py
+++ sql_queries.py
@@ -7,35 +7,56 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events (
+
+)
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs (
+
+)
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+
+)
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+
+)
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+
+)
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artist (
+
+)
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+
+)
 """)
 
 # STAGING TABLES
