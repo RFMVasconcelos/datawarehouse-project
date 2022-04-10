505daf2b80644f1c3ff47629ecc72bb738329c73
rui <rui.vasconcelos.mail@gmail.com>
2022-04-10 18:58:22 +0000

    Added jupyter notebook for tests

--- .ipynb_checkpoints/sql_queries-checkpoint.py
+++ .ipynb_checkpoints/sql_queries-checkpoint.py
@@ -19,13 +19,40 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 staging_events_table_create= ("""
 CREATE TABLE IF NOT EXISTS staging_events (
-
+event_id serial PRIMARY KEY,
+artist varchar,
+auth varchar,
+firstName varchar,
+gender varchar,
+itemInSession int,
+lastName varchar,
+length float,
+level varchar,
+location varchar, 
+method varchar,
+page varchar,
+registration float,
+sessionId int,
+song varchar, 
+status int,
+ts timestamp,
+userAgent varchar, 
+userId int
 )
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs (
-
+num_songs smallint,
+artist_id varchar,
+artist_latitude varchar,
+artist_longitude varchar,
+artist_location varchar,
+artist_name varchar,
+song_id varchar,
+title varchar,
+duration float,
+year smallint,
 )
 """)
 
@@ -88,10 +115,16 @@ weekday varchar
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    COPY staging_events FROM 's3://udacity-dend/log_data'
+    credentials 'aws_iam_role=ARN'
+    region 'us-west-2';
+""").format(ARN)
 
 staging_songs_copy = ("""
-""").format()
+    COPY staging_songs FROM 's3://udacity-dend/song_data'
+    credentials 'aws_iam_role=ARN'
+    region 'us-west-2';
+""").format(ARN)
 
 # FINAL TABLES
 
@@ -113,6 +146,9 @@ time_table_insert = ("""
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
 copy_table_queries = [staging_events_copy, staging_songs_copy]
+
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
