355a1b36e468ecff27018118ee0072542ed9d89c
rui <rui.vasconcelos.mail@gmail.com>
2022-04-13 13:08:12 +0000

    Added INSERT INTO statements

--- sql_queries.py
+++ sql_queries.py
@@ -1,6 +1,5 @@
 import configparser
 
-
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
@@ -19,7 +18,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 staging_events_table_create= ("""
 CREATE TABLE IF NOT EXISTS staging_events (
-event_id int,
+event_id IDENTITY(0,1),
 artist varchar,
 auth varchar,
 firstName varchar,
@@ -129,18 +128,47 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplays
+(
+start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users 
+(
+user_id, first_name, last_name, gender, level
+)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE SET level = excluded.level;
 """)
 
 song_table_insert = ("""
+INSERT INTO songs 
+(
+song_id, title, artist_id, year, duration
+)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists 
+(
+artist_id, name, location, latitude, longitude
+)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 time_table_insert = ("""
+INSERT INTO time 
+(
+start_time, hour, day, week, month, year, weekday
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s) 
+ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # QUERY LISTS
